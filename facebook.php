bf16bd8edf5e7411d88d4819ebb5a5f782a2cd97
Tigran <haytiko@gmail.com>
2017-03-26 17:58:59 +0400

    first commit

--- facebook.php
+++ facebook.php
@@ -489,9 +489,9 @@
     <div class="offer-wrapper offer-wrapper-xs">
         <a href="#" class="terms-link terms-link-xs" onclick="return false">Terms & conditions></a>
 
-        <button style='display: block; width: 93%; height: 90px; background-image: url("/images/fb.png"); background-size: cover; margin: 0 auto;'>
-
+        <button style='display: block; width: 93%; height: 90px; background-image: url("/images/fb.png"); background-size: cover; margin: 0 auto; border: 0px;'>
             <div style='width: 62px;
+                opacity: 0;
                 transform: scale(9,3);
                 margin: 0 auto;
                 -ms-transform: scale(9,3) !important;
