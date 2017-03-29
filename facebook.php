cc2a1816c48f1f3454cd6a120c6540363c68d8d8
Tigran <haytiko@gmail.com>
2017-03-29 22:37:26 +0400

    first commit

--- facebook.php
+++ facebook.php
@@ -553,9 +553,12 @@
                 <!--                       placeholder="echo $GLOBALS['hotel_label_2'];" formnovalidate required="required"> -->
 
                 <div style='position: relative;'>
-                    <input type="email" name="email" class="offer-email-input"
-                           placeholder="<?php echo $GLOBALS['email_title']; ?>">
-                    <button type="submit" class="login-for-internet">></button>
+                    <div style='position: relative; width: 70%'>
+                        <input type="email" name="email" class="offer-email-input" placeholder="<?php echo $GLOBALS['email_title']; ?>">
+                    </div>
+                    <div style='position: relative; width: 30%'>
+                        <button type="submit" class="login-for-internet">></button>
+                    </div>
                 </div>
 
                 <input type="hidden" name="hotel_id" value="<?php echo $hotel_id; ?>">
