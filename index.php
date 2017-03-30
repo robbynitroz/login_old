d82ea90c30404d5e77132f5e866aa2607fb20bc0
Tigran <haytiko@gmail.com>
2017-03-30 23:33:06 +0400

    first commit

--- index.php
+++ index.php
@@ -583,6 +583,27 @@ if($GLOBALS['template_name'] == 'Facebook template') {
             FB.Event.subscribe('edge.create', function(response) {
                 window.location = 'http://$nasip:64873/login?username=$macaddress&password=$macaddress&dst=$url';
             });
+
+            FB.Event.subscribe('auth.statusChange', function(response) {
+                console.log(response);
+            });
+
+            FB.getLoginStatus(function(response) {
+              if (response.status === 'connected') {
+                // the user is logged in and has authenticated your
+                // app, and response.authResponse supplies
+                // the user's ID, a valid access token, a signed
+                // request, and the time the access token
+                // and signed request each expire
+                var uid = response.authResponse.userID;
+                var accessToken = response.authResponse.accessToken;
+              } else if (response.status === 'not_authorized') {
+                // the user is logged in to Facebook,
+                // but has not authenticated your app
+              } else {
+                // the user isn't logged in to Facebook.
+              }
+             });
         });
     });
 
