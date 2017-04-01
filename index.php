b55950821fe9472c234c60b131d4579ba52ebe64
Tigran <haytiko@gmail.com>
2017-04-01 22:52:41 +0400

    first commit

--- index.php
+++ index.php
@@ -5,36 +5,15 @@ require 'Facebook/autoload.php';
 $fb = new Facebook\Facebook([
     'app_id' => '696113500523537',
     'app_secret' => 'f7c94fe5f0f51cc9a04fc2512b5c58cd',
-    'default_graph_version' => 'v2.5',
+    'default_graph_version' => 'v2.8',
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
-var_dump($helper);exit;
-try {
-    $accessToken = $helper->getAccessToken();
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-}
+$permissions = ['email', 'user_likes']; // optional
+$loginUrl = $helper->getLoginUrl('http://radiusdev.guestcompass.nl/index.php', $permissions);
+var_dump($loginUrl);exit;
 
-if (! isset($accessToken)) {
-    if ($helper->getError()) {
-        header('HTTP/1.0 401 Unauthorized');
-        echo "Error: " . $helper->getError() . "\n";
-        echo "Error Code: " . $helper->getErrorCode() . "\n";
-        echo "Error Reason: " . $helper->getErrorReason() . "\n";
-        echo "Error Description: " . $helper->getErrorDescription() . "\n";
-    } else {
-        header('HTTP/1.0 400 Bad Request');
-        echo 'Bad request';
-    }
-    exit;
-}
+
 error_reporting(E_ERROR);
 
 $language_codes = [
