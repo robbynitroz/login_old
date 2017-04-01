bc709bfb499b982a7ea1693ea12665b4552080c3
Tigran <haytiko@gmail.com>
2017-04-01 23:01:41 +0400

    first commit

--- index.php
+++ index.php
@@ -9,9 +9,28 @@ $fb = new Facebook\Facebook([
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
-$permissions = ['email', 'user_likes']; // optional
-$loginUrl = $helper->getLoginUrl('http://radiusdev.guestcompass.nl/index.php', $permissions);
-var_dump($loginUrl);exit;
+try {
+    $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+
+if (isset($accessToken)) {
+    // Logged in!
+    $_SESSION['facebook_access_token'] = (string) $accessToken;
+
+    // Now you can redirect to another page and use the
+    // access token from $_SESSION['facebook_access_token']
+}
+
+
+var_dump($accessToken);exit;
 
 
 error_reporting(E_ERROR);
