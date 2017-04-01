352c1e580adc97897de8e8b1ae6ffc54dd10a82e
Tigran <haytiko@gmail.com>
2017-04-01 22:46:07 +0400

    first commit

--- index.php
+++ index.php
@@ -7,26 +7,33 @@ $fb = new Facebook\Facebook([
     'default_graph_version' => 'v2.8',
 ]);
 
-$helper = $fb->getJavaScriptHelper();
+$helper = $fb->getRedirectLoginHelper();
 
 try {
     $accessToken = $helper->getAccessToken();
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
     // When Graph returns an error
     echo 'Graph returned an error: ' . $e->getMessage();
-//    exit;
+    exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
     // When validation fails or other local issues
     echo 'Facebook SDK returned an error: ' . $e->getMessage();
-//    exit;
+    exit;
 }
 
 if (! isset($accessToken)) {
-    echo 'No cookie set or no OAuth data could be obtained from cookie.';
-//    exit;
+    if ($helper->getError()) {
+        header('HTTP/1.0 401 Unauthorized');
+        echo "Error: " . $helper->getError() . "\n";
+        echo "Error Code: " . $helper->getErrorCode() . "\n";
+        echo "Error Reason: " . $helper->getErrorReason() . "\n";
+        echo "Error Description: " . $helper->getErrorDescription() . "\n";
+    } else {
+        header('HTTP/1.0 400 Bad Request');
+        echo 'Bad request';
+    }
+    exit;
 }
-
-print_r($accessToken);
 error_reporting(E_ERROR);
 
 $language_codes = [
