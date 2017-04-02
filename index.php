2aa8a70c6cd2dfbd02fb8f3986f1733034911cc6
Tigran <haytiko@gmail.com>
2017-04-02 16:33:12 +0400

    first commit

--- index.php
+++ index.php
@@ -618,7 +618,26 @@ if($GLOBALS['template_name'] == 'Facebook template') {
             } );
 
         });
+
+        getCookie('c_user');
+
     });
+
+    function getCookie(cname) {
+        var name = cname + '=';
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    }
 
 </script>";
 }
