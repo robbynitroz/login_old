4bf5a437356e4a5668085aa2f8ecdeddb2754d1b
Tigran <haytiko@gmail.com>
2017-04-02 15:25:14 +0400

    first commit

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
-session_start();
-require 'Facebook/autoload.php';
-
-$fb = new Facebook\Facebook([
-    'app_id' => '696113500523537',
-    'app_secret' => 'f7c94fe5f0f51cc9a04fc2512b5c58cd',
-    'default_graph_version' => 'v2.8',
-]);
-
-$helper = $fb->getRedirectLoginHelper();
-$permissions = ['email', 'user_likes']; // optional
-$loginUrl = $helper->getLoginUrl('http://{your-website}/login-callback.php', $permissions);
-
-echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-exit;
-
-
 error_reporting(E_ERROR);
 
 $language_codes = [
