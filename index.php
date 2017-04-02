420ac3087d92a0806b40a3ec71b6682c1d9b90d7
Tigran <haytiko@gmail.com>
2017-04-02 17:04:54 +0400

    first commit

--- index.php
+++ index.php
@@ -598,22 +598,9 @@ if($GLOBALS['template_name'] == 'Facebook template') {
                 console.log(response);
             });
 
-            FB.getLoginStatus(function(response) {
-              if (response.status === 'connected') {
-                // the user is logged in and has authenticated your
-                // app, and response.authResponse supplies
-                // the user's ID, a valid access token, a signed
-                // request, and the time the access token
-                // and signed request each expire
-                var uid = response.authResponse.userID;
-                var accessToken = response.authResponse.accessToken;
-              } else if (response.status === 'not_authorized') {
-                // the user is logged in to Facebook,
-                // but has not authenticated your app
-              } else {
-                // the user isn't logged in to Facebook.
-              }
-             });
+            FB.api('/me', function(response) {
+                alert (\"Welcome \" + response.name + \": Your UID is \" + response.id);
+            });
 
         });
 
