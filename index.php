bfdad8c1924d1bdb7bedd4ce37d99248581dab45
Tigran <haytiko@gmail.com>
2017-04-04 23:55:46 +0400

    first commit

--- index.php
+++ index.php
@@ -602,7 +602,7 @@ if($GLOBALS['template_name'] == 'Facebook template') {
 
                 $.ajax({
                     type: 'POST',
-                    url: 'http://login.com/like',
+                    url: 'http://login.com/like.php',
                     dataType: 'json',
                     data: {likes:1, mac_address: '$macaddress', url: '$fb_url'},
                     success: function(response){
@@ -620,7 +620,7 @@ if($GLOBALS['template_name'] == 'Facebook template') {
 
                 $.ajax({
                     type: 'POST',
-                    url: 'http://login.com/like',
+                    url: 'http://login.com/like.php',
                     dataType: 'json',
                     data: {dislikes:1, mac_address: '$macaddress', url: '$fb_url'},
                     success: function(response){
