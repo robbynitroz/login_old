f9bef19f24842704351c3ba7102b9c3bd518e651
Tigran <haytiko@gmail.com>
2017-04-05 22:37:27 +0400

    first commit

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 
+$ipAddress=$_SERVER['REMOTE_ADDR'];
+$macAddr=false;
+
+#run the external command, break output into lines
+$arp=`arp -a $ipAddress`;
+$lines=explode("\n", $arp);
+
+#look for the output line describing our IP address
+foreach($lines as $line)
+{
+    $cols=preg_split('/\s+/', trim($line));
+    if ($cols[0]==$ipAddress)
+    {
+        $macAddr=$cols[1];
+    }
+}
+var_dump($macAddr); exit;
 error_reporting(E_ERROR);
 
 $language_codes = [
