c584550abbe76401a81a99f7410606ad9e418973
Tigran <haytiko@gmail.com>
2017-04-07 23:02:07 +0400

    first commit

--- index.php
+++ index.php
@@ -25,13 +25,10 @@ if ($macaddress == '') {
     exit;
 }
 
-$link = mysqli_connect('localhost', 'root', 'Zq4F3R607h1K', 'radius');
+$link = mysql_connect('localhost', 'root', 'Zq4F3R607h1K') or die('Connection failed ' . mysql_error());
 
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-exit;
+mysql_select_db('radius') or die('DB selection failed');
+
 $query = 'SELECT  COUNT(username)  FROM radcheck where username="' . $macaddress . '" and attribute="User-Password"';
 $result = mysql_query($query) or die('Radius query error ' . mysql_error());
 $myrow = mysql_fetch_array($result);
