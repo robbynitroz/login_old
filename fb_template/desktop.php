9d32025c93a0ce597d19b50fdb811c73991ed4e2
Tigran <haytiko@gmail.com>
2017-04-09 20:03:58 +0400

    first commit

--- fb_template/desktop.php
+++ fb_template/desktop.php
@@ -102,8 +102,8 @@
 					<h1 class="heading"><?php echo $GLOBALS['title']; ?></h1>
 					<div class="form-w3step1">
 						<div class="social-wrap c">
-							<button type='button' class="facebook">Connect using Facebook
-								<a href="<?php echo $loginUrl; ?>"></a>
+							<button type='button' class="facebook">
+								<a href="<?php echo $loginUrl; ?>">Connect using Facebook</a>
 							</button>
 
 						</div>
