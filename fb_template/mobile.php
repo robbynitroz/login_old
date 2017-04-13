1d38e4bfb3c533913297e92de8fa8010a6587c52
Tigran <haytiko@gmail.com>
2017-04-14 00:19:50 +0400

    first commit

--- fb_template/mobile.php
+++ fb_template/mobile.php
@@ -87,21 +87,81 @@
 		<a href="#"><p class="terms">terms & conditions ></p></a>
 
 			<h1 class="heading">Go online with WiFi </h1>
-			<div class="form-w3step1">
-				<div class="social-wrap c">
-					<button class="facebook">Connect using Facebook</button>
+<!--			<div class="form-w3step1">-->
+<!--				<div class="social-wrap c">-->
+<!--					<button class="facebook">Connect using Facebook</button>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		<form action="#" method="post">-->
+<!--			<p class="subtitle fancy"><span>Or use your email</span></p>-->
+<!--			<div class="form-w3step1">-->
+<!---->
+<!--				<input type="email" class="email agileits-btm email-special" name="Email" placeholder="Your email address here">-->
+<!--				<button class="butt"> > </button>-->
+<!--			</div>-->
+<!--		</form>-->
 
+		<form action="http://login.com/emailSave.php" method="post">
+			<h1 class="heading"><?php echo $GLOBALS['title']; ?></h1>
+				<?php
+					if ($_GET['liked'] == 'false'):
+				?>
+				<div class="form-w3step1">
+					<div id='like_wrapper' style='margin-left: 40%;
+								transform: scale(2.5, 1.7);
+								-ms-transform: scale(2.5, 1.7);
+								-webkit-transform: scale(2.5, 1.7);
+								-o-transform: scale(2.5, 1.7);
+								-moz-transform: scale(2.5, 1.7);
+								'
+						 class="fb-like social-wrap c" data-href="<?php echo $fb_url; ?>"
+						 data-width="400"
+						 data-layout="button"
+						 data-action="like"
+						 data-size="large"
+						 data-show-faces="false"
+						 data-share="false">
+					</div>
 				</div>
+				<?php
+			else:
+				?>
+				<div class="form-w3step1">
+					<div class="social-wrap c">
+						<button type='button' class="facebook">
+							<a style='color: #ffffff;'
+							   href="http://fbdev.guestcompass.nl/index.php?macaddress=<?php echo $macaddress; ?>&nasip=<?php echo $nasip; ?>&hotel_id=<?php echo $hotel_id; ?>&url=<?php echo $url; ?>">
+								Connect using Facebook
+							</a>
+						</button>
+					</div>
+				</div>
+				<?php
+			endif;
+			?>
 
-			</div>
-		<form action="#" method="post">
-			<p class="subtitle fancy"><span>Or use your email</span></p>
+			<input type="hidden" id="user_email" value="<?php echo urldecode($_GET['email']); ?>">
+
+			<input type="hidden" name="hotel_id" value="<?php echo $hotel_id; ?>">
+			<input type="hidden" name="template_id" value="<?php echo $template_id; ?>">
+			<input type="hidden" name="nasip" value="<?php echo $nasip; ?>">
+			<input type="hidden" name="macaddress" value="<?php echo $macaddress; ?>">
+			<input type="hidden" name="url" value="<?php echo $url; ?>">
+
+			<p class="subtitle fancy"><span><?php echo $GLOBALS['middle_title']; ?></span></p>
 			<div class="form-w3step1">
-
-				<input type="email" class="email agileits-btm email-special" name="Email" placeholder="Your email address here">
+				<input type="email" class="email agileits-btm email-special" name="Email" placeholder="<?php echo $GLOBALS['email_title']; ?>">
 				<button class="butt"> > </button>
 			</div>
+
+			<div class="form-w3step1 logo-image">
+				<img src="images/<?php echo $GLOBALS['image']; ?>">
+			</div>
+
+			<!--<input type="submit" value="SUBMIT">-->
 		</form>
+
+
 			<div class="form-w3step1 logo-image">
 
 				<img src="images/<?php echo $GLOBALS['image']; ?>">
