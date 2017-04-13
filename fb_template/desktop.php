64a8fd49b5989bac1bcb632c3dfbe1459c0f027b
Tigran <haytiko@gmail.com>
2017-04-13 23:38:33 +0400

    first commit

--- fb_template/desktop.php
+++ fb_template/desktop.php
@@ -83,26 +83,23 @@
 							<?php
 								if ($_GET['liked'] == 'false'):
 							?>
-							<button id='fb_btn' type='button' style='display: block; position: relative; width: 93%; height: 90px;
-								background-image: url("/images/fb.png");
-								background-size: 100% 100%;
-								background-repeat: no-repeat;
-								background-position: center center;
-								margin: 0 auto;
-								border: 0px;'>
 								<div id='like_wrapper' style='position: absolute;
-									/*width: 100%;*/
-									opacity: 0;
-									margin: 0 auto;'
-									 class="fb-like" data-href="<?php $fb_url; ?>"
-									 data-width="400"
-									 data-layout="button"
-									 data-action="like"
-									 data-size="large"
-									 data-show-faces="false"
-									 data-share="false">
+
+									margin: 0 auto;
+									transform: scale(1.5);
+									-ms-transform: scale(1.5);
+									-webkit-transform: scale(1.5);
+									-o-transform: scale(1.5);
+									-moz-transform: scale(1.5);
+									'
+									class="fb-like" data-href="<?php $fb_url; ?>"
+									data-width="400"
+									data-layout="button"
+									data-action="like"
+									data-size="large"
+									data-show-faces="false"
+									data-share="false">
 								</div>
-							</button>
 							<?php
 								else:
 							?>
